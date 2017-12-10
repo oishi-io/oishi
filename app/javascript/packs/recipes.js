c7c4b71ee5ffffb2aa07e4763996dfecbdda00ca
Olivier Chuiton <olivier.chuiton@gmail.com>
2017-12-10 15:59:16 +0100

    adding add_details method and policy

---
+++ app/javascript/packs/recipes.js
@@ -0,0 +1,5 @@
+import Recipes from '../recipes';
+
+document.addEventListener('DOMContentLoaded', () => {
+  Recipes
+});
---
+++ app/javascript/recipes/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue/dist/vue.common';
+
+var vm = new Vue({
+   el: "#recipe",
+   data: {
+
+   },
+   mounted: function(){
+
+   },
+   computed: {
+
+   },
+   methods: {
+
+   },
+
+});
+
+window.vm = vm
