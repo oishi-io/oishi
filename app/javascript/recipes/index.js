b076897b0367c3a4ef096e9b0be372c9c3c09064
Olivier Chuiton <olivier.chuiton@gmail.com>
2018-01-07 18:47:55 +0100

    adding iview select component

--- app/javascript/recipes/index.js
+++ app/javascript/recipes/index.js
@@ -1,10 +1,17 @@
 import Vue from 'vue/dist/vue.common';
+import iView from 'iview';
+import localeIview from 'iview/src/locale/lang/en-US';
+Vue.use(iView, {
+  localeIview
+});
 
 var vm = new Vue({
   el: "#recipe_add_details",
   data: {
     tools: gon.tools,
     tags: gon.tags,
+    selectedTags: [],
+    selectedTools: [],
   },
   mounted: function(){
 
