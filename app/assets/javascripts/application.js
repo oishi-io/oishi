10c7b408c900992212042c2c57bbdcd1612bc253
Olivier Chuiton <olivier.chuiton@gmail.com>
2018-01-31 09:25:31 +0100

    rien

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,7 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require jquery-fileupload/basic
-//= require jquery.iframe-transport
+// = require jquery.iframe-transport
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
 //= require_tree .
