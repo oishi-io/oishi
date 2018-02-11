e3947507c83e59b62de671b0e46d93f8ba070a34
Olivier Chuiton <olivier.chuiton@gmail.com>
2018-02-11 11:27:41 +0100

    roll back to attachinary and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171217210049) do
+ActiveRecord::Schema.define(version: 20180210205325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20171217210049) do
     t.integer  "preparation_time"
     t.string   "difficulty"
     t.integer  "servings"
+    t.string   "photo"
     t.index ["user_id"], name: "index_recipes_on_user_id", using: :btree
   end
 
