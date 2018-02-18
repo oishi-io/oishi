a7c21c00bbe95ea2edb5e656b9954a9825b60e44
Olivier Chuiton <olivier.chuiton@gmail.com>
2018-02-18 21:09:07 +0100

    removing element from vm.measures after ajax

--- app/javascript/recipes/index.js
+++ app/javascript/recipes/index.js
@@ -118,21 +118,15 @@ var vm = new Vue({
       let _this = this;
       _this.newMeasure = measure;
     },
-    destroyMeasure(measure){
+    destroyMeasure(measure, index){
       let _this = this;
       $.ajax({
         method: 'DELETE',
         url: '/recipes/' + _this.recipeId + '/measures/' + measure.measure_id,
-        // data: {
-        //   name: tool.name,
-        // },
-        success: function(data){
-          console.log(data)
+        success: function(){
+          Vue.delete(vm.measures, measure.measure_id)
         }
       })
-          console.log(vm.measures[measure.measure_id])
-         vm.measures.delete(measure.measure_id);
-         vm.measures.splice(index, 1)
     },
   },
 });
