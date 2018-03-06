c7cbf2eb41f23b437b38d1faaa519ac0f63ea3ee
Olivier Chuiton <olivier.chuiton@gmail.com>
2018-03-06 21:51:37 +0100

    find or create with id only

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,6 +1,6 @@
 class StepsController < ApplicationController
   def create
-    @step = Step.find_or_create_by(id: params[:step_id], recipe_id: params[:recipe_id])
+    @step = Step.find_or_create_by(id: params[:step_id])
     @step.update(text: params[:text], index: params[:index])
     authorize @step
     @step.save
