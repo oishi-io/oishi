69e72d37e1403635c3ad20bdfe4a7baba69ed70c
Olivier Chuiton <olivier.chuiton@gmail.com>
2018-03-06 22:58:21 +0100

    fucking pundit

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,9 +1,10 @@
 class StepsController < ApplicationController
   def create
-    if params[:step_id] != ""
+    @step = Step.new
+    if params[:text] != "" && params[:index] != "" && params[:step_id] != ""
       @step = Step.find_by(id: params[:step_id])
       @step.update(text: params[:text], index: params[:index])
-    else
+    elsif params[:text] != "" && params[:index] != ""
       @step = Step.create(recipe_id: params[:recipe_id], text: params[:text], index: params[:index])
     end
     authorize @step
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,5 @@
 class Step < ApplicationRecord
   belongs_to :recipe
+  validates :index, presence: true
+  validates :text, presence: true
 end
