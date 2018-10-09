e94f4c469bdc07a34728e60a90916cae89b07bbf
olivier chuiton <olivier.chuiton@gmail.com>
2018-10-09 21:44:13 +0200

    dynamic left margin calculation

--- app/javascript/home/index.js
+++ app/javascript/home/index.js
@@ -8,12 +8,27 @@ const vm = new Vue({
     typingTimer: 0,
     typingInterval: 500,
     isLoading: false,
+    windowWidth: 0,
+    leftMargin: 0,
+    recipesCount: 0,
   },
   watch: {
     query() {
       clearTimeout(this.typingTimer);
       this.typingTimer = setTimeout(() => this.searchRecipes(), this.typingInterval);
     },
+    windowWidth() {
+      this.getLeftMargin();
+    },
+  },
+  mounted() {
+    this.windowWidth = window.innerWidth;
+    this.recipesCount = this.recipes.length;
+    this.$nextTick(() => {
+      window.addEventListener('resize', () => {
+        this.windowWidth = window.innerWidth;
+      });
+    })
   },
   components: {
   },
@@ -32,11 +47,22 @@ const vm = new Vue({
         success: function(data) {
           _this.isLoading = false;
           _this.recipes = data.recipes;
+          _this.recipesCount = _this.recipes.length;
+          _this.getLeftMargin();
         }
       })
     },
     getScreenHeight() {
-      return `${screen.availHeight - 10}px`;
+      return `${screen.availHeight}px`;
+    },
+    getLeftMargin() {
+      const scrollContainerWidth = (this.recipesCount * 250) + ((this.recipesCount - 1) * 20);
+      console.log('container: ', scrollContainerWidth)
+      const margin = this.windowWidth - scrollContainerWidth;
+      console.log('rawMargin: ', margin)
+
+      this.leftMargin = margin > 0 ? `${(margin/2) - 5}px` : 0;
+      console.log(this.leftMargin)
     },
   },
 });
