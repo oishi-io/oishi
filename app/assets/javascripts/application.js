99a859cd31851fece0440afdde314d575c2280f6
olivier chuiton <olivier.chuiton@gmail.com>
2018-10-11 22:26:19 +0200

    remove jquery.iframe-transport

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,8 +2,6 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require jquery-fileupload/basic
-// = require jquery.iframe-transport
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
 //= require_tree .
-
