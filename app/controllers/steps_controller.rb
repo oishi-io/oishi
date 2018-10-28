0a05f04207f31871612ff772c97be44ba0450308
olivier chuiton <olivier.chuiton@gmail.com>
2018-10-28 15:57:53 +0100

    refacto steps create/update

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,19 +1,26 @@
 class StepsController < ApplicationController
   def create
-    @step = Step.new
-    if params[:text] != '' && params[:order] != '' && params[:step_id] != ''
-      @step = Step.find_by(id: params[:step_id])
-      @step.update(text: params[:text], order: params[:order])
-    elsif params[:text] != '' && params[:order] != ''
-      @step = Step.create(recipe_id: params[:recipe_id], text: params[:text], order: params[:order])
+    return unless params[:text].present? && params[:order].present?
+
+    if params[:step_id] != ''
+      @step = Step.find(params[:step_id])
+      @step.update(
+        text: params[:text],
+        order: params[:order]
+      )
+    else
+      @step = Step.create(
+        recipe_id: params[:recipe_id],
+        text: params[:text],
+        order: params[:order]
+      )
     end
     authorize @step
-    @step.save
     render json: { step: @step, status: 200 }
   end
 
   def destroy
-    @step = Step.find(params[:id]) # get the step from the id
+    @step = Step.find(params[:id])
     @step.destroy
     authorize @step
     render json: @step
